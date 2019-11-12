7eb0e942223eec6d8ad26ad40edc22903649b983
Alyona Fomenkova <fruitella30sep@mail.ru>
2019-11-12 18:30:55 +0300

    Adding rendering cards.

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 //import {getRandomNumber} from './util.js';
 import makeFilter from './make-filter.js';
+import makeCard from './make-card.js';
 
 const FILTERS = [
   {
@@ -20,7 +21,26 @@ const FILTERS = [
     count: 8
   }
 ];
+const FILM = {
+  title: `Moonrise`,
+  rating: `6.1`,
+  year: `1948`,
+  duration: `1h 30m`,
+  genre: `Nuar`,
+  description: `Danny Hawkins, has a tragic past. He makes an unintentional murder,
+                saving the girl from the hands of scoundrels. Facing a painful choice,
+                he tries to flee not only from the police, but from himself.`,
+  comments: 7
+};
+const CountOfFilms = {
+  COMMON: 7,
+  EXTRA: 2
+};
 const filtersContainer = document.querySelector(`.main-navigation`);
+const filmsContainers = document.querySelectorAll(`.films-list__container`);
+const commonFilmsContainer = filmsContainers[0];
+const topRatedFilmsContainer = filmsContainers[1];
+const mostCommentedFilmsContainer = filmsContainers[2];
 
 const renderFilters = (filters) => {
   filters.reverse().forEach((item) => {
@@ -28,6 +48,16 @@ const renderFilters = (filters) => {
   });
 };
 
+const renderCards = (container, count, film, isControls = false) => {
+  for (let i = 0; i < count; i++) {
+    container.insertAdjacentHTML(`beforeend`,
+    makeCard(film.title, film.rating, film.year, film.duration, film.genre, film.description, film.comments, isControls));
+  }
+};
+
 renderFilters(FILTERS);
+renderCards(commonFilmsContainer, CountOfFilms.COMMON, FILM, true);
+renderCards(topRatedFilmsContainer, CountOfFilms.EXTRA, FILM);
+renderCards(mostCommentedFilmsContainer, CountOfFilms.EXTRA, FILM);
 
 //getRandomNumber();
---
+++ src/make-card.js
@@ -0,0 +1,23 @@
+export default (title, rating, year, duration, genre, description, comments, isControls = false) => {
+  return `
+  <article class="film-card" class="film-card ${isControls ? `` : ` film-card--no-controls`}">
+    <h3 class="film-card__title">${title}</h3>
+    <p class="film-card__rating">${rating}</p>
+    <p class="film-card__info">
+      <span class="film-card__year">${year}</span>
+      <span class="film-card__duration">${duration}</span>
+      <span class="film-card__genre">${genre}</span>
+    </p>
+    <img src="./images/posters/${title.toLowerCase().split(` `).join(`-`)}.jpg" alt="" class="film-card__poster">
+    <p class="film-card__description">${description}</p>
+    <button class="film-card__comments">${comments} comments</button>
+    ${isControls ? `
+      <form class="film-card__controls">
+        <button class="film-card__controls-item button film-card__controls-item--add-to-watchlist">Add to watchlist</button>
+        <button class="film-card__controls-item button film-card__controls-item--mark-as-watched">Mark as watched</button>
+        <button class="film-card__controls-item button film-card__controls-item--favorite">Mark as favorite</button>
+      </form>`
+    : ``}
+  </article>
+  `;
+};
