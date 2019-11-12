c78a0bdbaed8ecbd25708868f43fbcf2e6398260
Alyona Fomenkova <fruitella30sep@mail.ru>
2019-11-12 19:46:55 +0300

    Adding a handler to switch the filters.

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-//import {getRandomNumber} from './util.js';
+import {getRandomNumber} from './util.js';
 import makeFilter from './make-filter.js';
 import makeCard from './make-card.js';
 
@@ -34,7 +34,9 @@ const FILM = {
 };
 const CountOfFilms = {
   COMMON: 7,
-  EXTRA: 2
+  EXTRA: 2,
+  MIN: 0,
+  MAX: 14
 };
 const filtersContainer = document.querySelector(`.main-navigation`);
 const filmsContainers = document.querySelectorAll(`.films-list__container`);
@@ -55,9 +57,29 @@ const renderCards = (container, count, film, isControls = false) => {
   }
 };
 
+const updateCards = () => {
+  const filtersList = filtersContainer.querySelectorAll(`.main-navigation__item`);
+  const onFiltersClick = (evt) => {
+    evt.preventDefault();
+    const target = evt.target;
+    const count = getRandomNumber(CountOfFilms.MIN, CountOfFilms.MAX);
+
+    commonFilmsContainer.innerHTML = ``;
+    renderCards(commonFilmsContainer, count, FILM, true);
+
+    filtersList.forEach((item) => {
+      item.classList.remove(`main-navigation__item--active`);
+    });
+    target.classList.add(`main-navigation__item--active`);
+  };
+
+  filtersList.forEach((item) => {
+    item.addEventListener(`click`, onFiltersClick);
+  });
+};
+
 renderFilters(FILTERS);
 renderCards(commonFilmsContainer, CountOfFilms.COMMON, FILM, true);
 renderCards(topRatedFilmsContainer, CountOfFilms.EXTRA, FILM);
 renderCards(mostCommentedFilmsContainer, CountOfFilms.EXTRA, FILM);
-
-//getRandomNumber();
+updateCards();
