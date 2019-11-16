cc57bcaf3413bb2b3d4feabe01762ac70dc5b88c
Alyona Fomenkova <fruitella30sep@mail.ru>
2019-11-16 19:35:07 +0300

    Updating render of cards with mock data.

--- src/make-card.js
+++ src/make-card.js
@@ -1,16 +1,16 @@
-export default (title, rating, year, duration, genre, description, comments, isControls = false) => {
+export default (film, isControls = false) => {
   return `
   <article class="film-card ${isControls ? `` : ` film-card--no-controls`}">
-    <h3 class="film-card__title">${title}</h3>
-    <p class="film-card__rating">${rating}</p>
+    <h3 class="film-card__title">${film.title}</h3>
+    <p class="film-card__rating">${film.rating}</p>
     <p class="film-card__info">
-      <span class="film-card__year">${year}</span>
-      <span class="film-card__duration">${duration}</span>
-      <span class="film-card__genre">${genre}</span>
+      <span class="film-card__year">${film.year}</span>
+      <span class="film-card__duration">${film.duration}</span>
+      <span class="film-card__genre">${film.genre}</span>
     </p>
-    <img src="./images/posters/${title.toLowerCase().split(` `).join(`-`)}.jpg" alt="" class="film-card__poster">
-    <p class="film-card__description">${description}</p>
-    <button class="film-card__comments">${comments} comments</button>
+    <img src="./images/posters/${film.posters}" alt="" class="film-card__poster">
+    <p class="film-card__description">${film.description}</p>
+    <button class="film-card__comments">${film.comments} comments</button>
     ${isControls ? `
       <form class="film-card__controls">
         <button class="film-card__controls-item button film-card__controls-item--add-to-watchlist">Add to watchlist</button>
