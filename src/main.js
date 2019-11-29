6357cf419e6c75a79ae2c3e7bbdf6e5621df3661
Alyona Fomenkova <fruitella30sep@mail.ru>
2019-11-29 20:17:18 +0300

    Changing renderFilms.

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 import {generatedFilms, CountOfFilms} from './data.js';
-import {getRandomNumber, getSubarray} from './util.js';
+import {getRandomNumber, getShuffledSubarray, getSubarray, createElement} from './util.js';
 import makeFilter from './make-filter.js';
 import {Film} from './film.js';
 import {FilmDetails} from './film-details.js';
 import {FilmStorage} from './film-storage.js';
+import {ElementBuilder} from './element-builder.js';
 
 const FILTERS = [
   {
@@ -30,21 +31,16 @@ const commonFilmsContainer = filmsContainers[0];
 const topRatedFilmsContainer = filmsContainers[1];
 const mostCommentedFilmsContainer = filmsContainers[2];
 
+const Group = {
+  ALL: 0,
+  TOP_RATED: 1,
+  MOST_COMMENTED: 2
+};
+
 const renderFilters = (filters) => {
   filters.reverse().forEach((item) => {
     filtersContainer.insertAdjacentHTML(`afterbegin`, makeFilter(item.name, item.count, item.isChecked));
   });
-};
-
-const renderFilms = (container, array) => {
-  array.forEach(element => {
-    const filmComponent = new Film(element);
-    container.appendChild(filmComponent.render());
-
-    const filmDetailsComponent = new FilmDetails(element);
-    filmComponent.onClick = () => {
-      body.appendChild(filmDetailsComponent.render());
-    };});
 };
 
 const updateFilms = () => {
@@ -70,40 +66,60 @@ const updateFilms = () => {
 };
 
 renderFilters(FILTERS);
-renderFilms(commonFilmsContainer, generatedFilms);
-renderFilms(topRatedFilmsContainer, generatedFilms);
-renderFilms(mostCommentedFilmsContainer, generatedFilms);
-updateFilms();
-
-
-const body = document.querySelector(`body`);
-//const filmComponent = new Film(generateFilm());
-//const filmDetailsComponent = new FilmDetails(generateFilm());
-// console.log(`filmComponent: `, filmComponent);
-// console.log(`filmDetailsComponent: `, filmDetailsComponent);
-
-//commonFilmsContainer.appendChild(filmComponent.render());
-
-// filmComponent.onClick = () => {
-//   body.appendChild(filmDetailsComponent.render());
-// };
-
-// filmDetailsComponent.onClick = () => {
-//   //filmDetailsComponent.render();
-//   //commonFilmsContainer.replaceChild(filmDetailsComponent.element, filmComponent.element);
-//   //filmComponent.unrender();
-//   body.removeChild(filmDetailsComponent);
-//   filmDetailsComponent.unrender()
-// };
+// updateFilms();
 
 const films = new FilmStorage();
+console.log(typeof films);
+console.log(`films: `, films);
+
+const renderFilms = (container, films, group) => {
+  const body = document.querySelector(`body`);
+
+  films.forEach(film => {
+    const overlay = ElementBuilder.createOverlay();
+
+    const onDetailedFilmClick = () => {
+      body.removeChild(detailedFilmComponent);
+      body.removeChild(overlay);
+    };
+
+    const onSmallFilmClick = () => {
+      body.appendChild(overlay);
+      body.appendChild(detailedFilmComponent);
+    }
+
+    let filmComponent;
+
+    switch (group) {
+      case Group.ALL:
+        filmComponent = ElementBuilder.buildSmallFilmElement(film, onSmallFilmClick)
+        break;
+      case Group.TOP_RATED:
+        filmComponent = ElementBuilder.buildExtraSmallFilmElement(film, onSmallFilmClick);
+        break;
+      case Group.MOST_COMMENTED:
+        filmComponent = ElementBuilder.buildExtraSmallFilmElement(film, onSmallFilmClick);
+        break;
+      default:
+        throw new Error(`Unknown group type: ${group}`);
+    }
+
+    const detailedFilmComponent = ElementBuilder.buildDetailedFilmElement(film, onDetailedFilmClick);
+    container.appendChild(filmComponent);
+  });
+}
 
 const onError = () => {
-  console.error(`Error loading films from server`);
+  console.log(`ERROR LOADING`);
 }
 
 const onSuccess = (films) => {
-  console.log(`Films onSuccess: `, films);
+  const topRatedFilms = getShuffledSubarray(films, CountOfFilms.EXTRA);
+  const mostCommentedFilms = getShuffledSubarray(films, CountOfFilms.EXTRA);
+
+  renderFilms(commonFilmsContainer, films, Group.ALL);
+  renderFilms(topRatedFilmsContainer, topRatedFilms, Group.TOP_RATED);
+  renderFilms(mostCommentedFilmsContainer, mostCommentedFilms, Group.MOST_COMMENTED);
 }
 
 films.load(onError, onSuccess);
